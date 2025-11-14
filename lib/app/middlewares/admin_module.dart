55901c56f17165e62a86a1a5b4ee357bcb8fb47c
keysyayst <keysyayesantisafaat@gmail.com>
2025-11-15 00:54:22 +0700

    fix problem

---
+++ lib/app/middlewares/admin_module.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../data/services/auth_service.dart';
+import '../routes/app_pages.dart';
+
+class AdminMiddleware extends GetMiddleware {
+  @override
+  RouteSettings? redirect(String? route) {
+    final authService = Get.find<AuthService>();
+    
+    if (!authService.isAdmin) {
+      Get.snackbar(
+        'Akses Ditolak',
+        'Anda tidak memiliki akses admin',
+        snackPosition: SnackPosition.BOTTOM,
+        backgroundColor: Colors.red,
+        colorText: Colors.white,
+      );
+      return const RouteSettings(name: Routes.dashboard);
+    }
+    
+    return null;
+  }
+}
