622dfe1c44dc1cec3c1be48ca4e524f9c53536e0
Irfan Deny <irfandeny112@gmail.com>
2025-11-15 17:54:43 +0700

    edit .env

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,28 @@
 ﻿import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'app/app.dart';
 import 'app/data/services/auth_service.dart';
 import 'app/data/sources/products.dart';
 import 'app/data/services/favorite_hive_service.dart';
 
 Future<void> initServices() async {
-  // Initialize Supabase
-  await Supabase.initialize(
-    url:
-        'https://sinjtvyyomaxsmaziczx.supabase.co', // ← GANTI dengan URL Supabase Anda
-    anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNpbmp0dnl5b21heHNtYXppY3p4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMwNDI2OTEsImV4cCI6MjA3ODYxODY5MX0.-d4k2sfwgYlQ13lBpFt9k4eO0_MYKXXiqF5GJoz_J_8', // ← GANTI dengan anon key Anda
-  );
+  // Ambil variabel environment
+  final supabaseUrl = dotenv.env['SUPABASE_URL'];
+  final supabaseAnonKey = dotenv.env['SUPABASE_ANON_KEY'];
+
+  if (supabaseUrl == null ||
+      supabaseUrl.isEmpty ||
+      supabaseAnonKey == null ||
+      supabaseAnonKey.isEmpty) {
+    throw Exception(
+      'Environment variables SUPABASE_URL atau SUPABASE_ANON_KEY tidak ditemukan. Pastikan file .env sudah dibuat.',
+    );
+  }
+
+  // Initialize Supabase menggunakan variabel .env
+  await Supabase.initialize(url: supabaseUrl, anonKey: supabaseAnonKey);
 
   // Initialize Hive
   final favoriteService = FavoriteHiveService();
@@ -28,6 +37,8 @@ Future<void> initServices() async {
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  // Load file .env
+  await dotenv.load(fileName: '.env');
   await initServices();
   runApp(const App());
 }
